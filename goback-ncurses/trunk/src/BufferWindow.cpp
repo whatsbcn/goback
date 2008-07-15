cdbc723b683afc6e3e0f4f5158be8b01bf488000
jvprat <jvprat@gmail.com>
2008-07-15 22:00:47 +0000

    - Added BufferWindow class (can you find a better name?) to handle the display of a WorkMode which can be resized to use just a small part of the screen - Added vertical cursor handling (move the cursor to the top and the bottom of the screen to scroll)
    git-svn-id: https://www.wekk.net/svn-goback@51 366799d8-7408-4d85-b325-958a656c2bda

---
+++ goback-ncurses/trunk/src/BufferWindow.cpp
@@ -0,0 +1,107 @@
+#include "BufferWindow.h"
+#include <WorkMode.h>
+
+#include <curses.h>
+
+BufferWindow::BufferWindow(unsigned int x, unsigned int y, unsigned int w, unsigned int h) :
+	_x(x), _y(y), _w(w), _h(h), _viewLine(0), _viewCol(0),
+	_cursorViewLine(0), _cursorViewCol(0), _wm(NULL) {
+}
+
+void BufferWindow::setWorkMode(WorkMode *wm) {
+	_wm = wm;
+	_numLines = _wm->getNumberLines();
+
+	// Update the window?
+	updateWindow();
+}
+
+unsigned int BufferWindow::getViewPercentage() {
+	if (_viewLine + _h >= _numLines) {
+		return 100;
+	} else {
+		return ((_viewLine + _h) * 100) / _numLines;
+	}
+}
+
+
+/*
+ * Cursor
+ */
+
+unsigned int BufferWindow::getCursorLine() {
+	return _viewLine + _cursorViewLine;
+}
+
+unsigned int BufferWindow::getCursorCol() {
+	return _viewCol + _cursorViewCol;
+}
+
+void BufferWindow::cursorMoveUp() {
+	if (_cursorViewLine > 0) {
+		// Move the cursor one line up on the screen
+		_cursorViewLine--;
+	} else {
+		// Already on the top line, move the contents
+		if (_viewLine > 0) {
+			_viewLine--;
+		}
+	}
+}
+
+void BufferWindow::cursorMoveDown() {
+	if (_cursorViewLine + 1 < _h) {
+		// Move the cursor one line down on the screen
+		_cursorViewLine++;
+	} else {
+		// Already on the bottom line, move the contents
+		if (_viewLine + _h < _numLines) {
+			_viewLine++;
+			// I don't understand why I have to invalidate this line when I go down...
+			wredrawln(stdscr, _h, 1);
+		}
+	}
+}
+
+void BufferWindow::cursorPageUp() {
+	if (_viewLine < _h) {
+		_viewLine = 0;
+	} else {
+		_viewLine -= _h;
+	}
+}
+
+void BufferWindow::cursorPageDown() {
+	if (_viewLine + _h * 2 > _numLines) {
+		_viewLine = _numLines - _h;
+		if (_viewLine < 0) {
+			_viewLine = 0;
+		}
+	} else {
+		_viewLine += _h;
+	}
+}
+
+void BufferWindow::showCursor() {
+	move(_cursorViewLine, 0);
+}
+
+void BufferWindow::updateWindow() {
+	for (int i = 0; i < _numLines && i < _h; i++) {
+		updateWindowLine(i);
+	}
+}
+
+// Update a line in the window.
+void BufferWindow::updateWindowLine(unsigned int numline) {
+	ViewLine line = _wm->getLine(_viewLine + numline);
+	move(_y + numline, _x);
+
+	// Print the blocks
+	ViewLine::iterator j = line.begin();
+	while (j != line.end()) {
+		printw("%s", j->_str.c_str());
+		clrtoeol(); // TODO: just clear until the end of the window
+		j++;
+	}
+}
---
+++ goback-ncurses/trunk/src/BufferWindow.h
@@ -0,0 +1,48 @@
+class WorkMode;
+
+class BufferWindow {
+public:
+	BufferWindow(unsigned int x, unsigned int y, unsigned int w, unsigned int h);
+	void setWorkMode(WorkMode *wm);
+
+	unsigned int getViewPercentage();
+
+	// Cursor
+	unsigned int getCursorLine();
+	unsigned int getCursorCol();
+	void cursorMoveUp();
+	void cursorMoveDown();
+	//void cursorMoveLeft();
+	//void cursorMoveRight();
+	//void cursorMoveBeginning();
+	//void cursorMoveEnd();
+	void cursorPageUp();
+	void cursorPageDown();
+	//void cursorSetPosition();
+	void showCursor();
+
+	void updateWindow();
+
+private:
+	// Window position and size
+	unsigned int _x, _y, _w, _h;
+
+	// View position
+	unsigned int _viewLine, _viewCol;
+
+	// Cursor position
+	unsigned int _cursorViewLine, _cursorViewCol;
+
+	// The WorkMode to show
+	WorkMode *_wm;
+	unsigned int _numLines;
+
+	// Helper functions
+	void updateWindowLine(unsigned int numline);
+
+	/* TODO:
+	 * Possible events:
+	 * - View Position changed
+	 * - Cursor Position changed
+	 */
+};
