a788c8903d44b9238a3dcb30fa5b7dd0745aebd6
jvprat <jvprat@gmail.com>
2008-07-15 11:19:49 +0000

    Remove spaces at the end of line and collapse contiguous space blocks into one
    git-svn-id: https://www.wekk.net/svn-goback@49 366799d8-7408-4d85-b325-958a656c2bda

--- libgoback/trunk/src/WorkModeHex.cpp
+++ libgoback/trunk/src/WorkModeHex.cpp
@@ -17,42 +17,38 @@ int WorkModeHex::getNumberLines() {
 ViewLine WorkModeHex::getLine(int line) {
 	ViewLine viewline;
 	char c[_lineBytes];
-	char buff[9];
+	char buff[11];
 
 	// Read the line
 	int bytes = _dataSource->readBytes(c, _lineBytes, line * _lineBytes);
 
-	sprintf(buff, "%08x:", line);
-	viewline.push_back(ViewBlock(buff, true));
-	viewline.push_back(ViewBlock(" ", false));
+	// Show the line number
+	// TODO: Move it to the client? Maybe add a function to return the line
+	// number to be shown for a given internal line number?
+	sprintf(buff, "%08x: ", line);
+	viewline.push_back(ViewBlock(buff, false));
 
 	// Show the hexadecimal values
 	for (int i = 0; i < bytes; i++) {
+		if (i != 0) {
+			// Add a space to separate from the previous hex block
+			viewline.push_back(ViewBlock(" ", false));
+		}
+
+		// Prepare the hexadecimal representation
 		sprintf(buff, "%02x", c[i]);
-		buff[2] = '\0';
 		viewline.push_back(ViewBlock(buff, true));
-		viewline.push_back(ViewBlock(" ", false));
-	}
-	// Fill the gaps with spaces
-	int gaps = _lineBytes - bytes;
-	for (int i = 0; i < gaps; i++) {
-		viewline.push_back(ViewBlock("  ", false));
-		viewline.push_back(ViewBlock(" ", false));
 	}
 
-	// Separation space
-	viewline.push_back(ViewBlock(" ", false));
+	// Add the empty positions filling and separation spaces
+	int gaps = (_lineBytes - bytes) * 3 + 2;
+	viewline.push_back(ViewBlock(std::string(gaps, ' '), false));
 
 	// ASCII representation
+	buff[1] = 0;
 	for (int i = 0; i < bytes; i++) {
-		sprintf(buff, "%c", isprint(c[i]) ? c[i] : '.');
-		buff[1] = '\0';
+		buff[0] = isprint(c[i]) ? c[i] : '.';
 		viewline.push_back(ViewBlock(buff, true));
-	}
-
-	// Fill the gaps with spaces
-	for (int i = 0; i < gaps; i++) {
-		viewline.push_back(ViewBlock(" ", false));
 	}
 
 	return viewline;
