dfc455d74d3a792d45b1d6d401d387bbbee4f9d5
seoman <marc.morata.fite@gmail.com>
2008-08-04 04:13:23 +0000

    Added Png DataFormat support
    git-svn-id: https://www.wekk.net/svn-goback@87 366799d8-7408-4d85-b325-958a656c2bda

---
+++ libgoback/trunk/src/DataFormatPng.h
@@ -0,0 +1,40 @@
+#ifndef DATAFORMATPng_H
+#define DATAFORMATPng_H
+
+#include "DataFormat.h"
+
+class DataFormatPngModule : public DataFormatModule {
+public:
+	// Identification
+	std::string id() const;
+	std::string name() const;
+
+	// Format detection
+	bool detect(DataSource *ds) const;
+
+	// DataFormat instantiation
+	DataFormat *create(DataSource *ds) const;
+
+};
+
+class DataFormatPng : public DataFormat {
+public:
+	DataFormatPng(DataSource *ds);
+	~DataFormatPng();
+
+	bool load();
+	static bool isPngFile(DataSource *ds); 
+
+protected:
+	struct PngSection {
+		std::string name;
+		unsigned int offset;
+		unsigned int size;
+		unsigned int address;
+		unsigned int type;
+	};
+
+	std::list<PngSection> _sections;
+};
+
+#endif // DATAFORMATPng_H
